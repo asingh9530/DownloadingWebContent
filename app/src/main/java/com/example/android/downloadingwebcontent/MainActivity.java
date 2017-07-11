8fe76157dd3f061bc32c3ae5a432ac3bcd6a6c1d
abhinavfreecodecamp <abhinav199530singh@gmail.com>
2017-07-11 18:48:04 +0530

    Checking Functionality

--- app/src/main/java/com/example/android/downloadingwebcontent/MainActivity.java
+++ app/src/main/java/com/example/android/downloadingwebcontent/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.android.downloadingwebcontent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-    public class Download extends AsyncTask<String,void,String >{
+    public class Download extends AsyncTask<String, Void ,String >{
 
+
+        @Override
+        protected String doInBackground(String... params) {
+            Log.i("URl",params[0]);
+
+            return "Done";
+        }
     }
 
 
@@ -15,5 +25,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Download task = new Download();
+        try {
+            String result= task.execute("https://www.ecowebhosting.co.uk/","http://www.stackoverflow.com").get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
